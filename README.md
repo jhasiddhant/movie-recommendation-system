681c52f92cfbc5c344d7210a3e37c10b73005d76
Siddhant Jha <jhasiddhant@hotmail.com>
2023-02-05 21:30:18 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
-# movie-recommendation-system
+# Content Based Movie Recommendation System
+
+This repository contains a movie recommendation system built using the TF-IDF and SVD techniques. The system is designed to recommend movies to users based on the similarity between the movies' descriptions, genres, keywords, cast and crew.
+
+## Introduction
+
+A movie recommendation system is a tool that suggests movies to users based on their preferences and interests. This project is a movie recommendation system that recommends movies to users based on the movies which they have already watched.
+
+## Requirements
+
+To run this recommendation system, you will need the following software:
+
+Python 3.9.12
+<br />
+Pandas
+<br />
+Numpy
+<br />
+Scikit-learn
+<br />
+NLTK
+
+## Datasets
+Source: https://www.kaggle.com/datasets/rounakbanik/the-movies-dataset
+<br />
+The dataset used in this project is composed of three files:
+
+movies_metadata.csv: Contains metadata of movies, including the title, id, overview, and genres.
+<br />
+keywords.csv: Contains keywords for each movie.
+<br />
+credits.csv: Contains information about the cast and crew of each movie.
+
+## Implementation
+
+The recommendation system is implemented in the recommendations.py file. The code performs the following steps:
+
+1. Load and clean the datasets.
+
+2. Preprocess the dataset, including removing stopwords, stemming and lemmatizing the words.
+
+3. Compute the TF-IDF scores for each movie description.
+
+4. Use Truncated SVD to reduce the dimensionality of the TF-IDF scores.
+
+5. Calculate the cosine similarity between each pair of movies.
+
+6. Make recommendations by finding the most similar movies to the movie of interest.
+
+## Installation
+
+Clone this repository to your local machine:
+
+```bash
+git clone https://github.com/jhasiddhant/movie-recommendation-system.git
+```
+Install the required packages:
+```bash
+pip install -r requirements.txt
+```
+Run the recommendation.py file:
+```bash
+python recommendation.py
+```
+When prompted, enter the name of a movie to receive recommendations for similar movies.
+
+## Example:
+Enter movie name: The lord of the rings
+
+Recommended Movies:
+<br />
+THE LORD OF THE RINGS: THE TWO TOWERS
+<br />
+THE LORD OF THE RINGS: THE FELLOWSHIP OF THE RING
+<br />
+THE RING THING
+<br />
+THE HUNT FOR GOLLUM
+<br />
+THE LORD OF THE RINGS: THE RETURN OF THE KING
+<br />
+THE RETURN OF THE KING
+<br />
+WOLF
+<br />
+THE HOBBIT: AN UNEXPECTED JOURNEY
+<br />
+THE HOBBIT: THE DESOLATION OF SMAUG
+<br />
+GOR
--- mrs.py
+++ mrs.py
@@ -1,9 +1,16 @@
-import pandas as pd
+import nltk
+import string
 import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics.pairwise import cosine_similarity
-from nltk.corpus import stopwords
+
+# Download required NLTK resources
+nltk.download("stopwords")
+nltk.download("wordnet")
 
 
 # Access Datasets
@@ -12,11 +19,12 @@ keywords_csv = pd.read_csv('./datasets/keywords.csv')
 movies_csv = pd.read_csv('./datasets/movies_metadata.csv', dtype='unicode')
 
 
-# Data Cleaning
+# Preprocess the datasets
 movies_csv = movies_csv[['id', 'original_title', 'overview', 'genres']]
 movies_csv['title'] = movies_csv['original_title']
 movies_csv.reset_index(inplace=True, drop=True)
 movies_csv.drop('original_title', axis=1, inplace=True)
+movies_csv['title'] = movies_csv['title'].apply(str.upper)
 
 movies_csv['id'] = movies_csv['id'].astype(int)
 dataset = pd.merge(movies_csv, keywords_csv, on='id', how='left')
@@ -24,58 +32,95 @@ dataset.reset_index(inplace=True, drop=True)
 dataset = pd.merge(dataset, credits_csv, on='id', how='left')
 dataset.reset_index(inplace=True, drop=True)
 
-dataset['genre'] = dataset['genres'].apply(lambda x: [i['name'] for i in eval(x)])
-dataset['genre'] = dataset['genre'].apply(lambda x: ' '.join([i.replace(" ", "") for i in x]))
-dataset.drop('genres', axis=1, inplace=True)
+def preprocess_column(df, column):
+    df[column].fillna('[]', inplace=True)
+    df[column] = df[column].apply(lambda x: [i['name'] for i in eval(x)])
+    df[column] = df[column].apply(lambda x: ' '.join([i.replace(" ", "") for i in x]))
+    return df
 
-dataset['keywords'].fillna('[]', inplace=True)
-dataset['keywords'] = dataset['keywords'].apply(lambda x: [i['name'] for i in eval(x)])
-dataset['keywords'] = dataset['keywords'].apply(lambda x: ' '.join([i.replace(" ", "") for i in x]))
+dataset = preprocess_column(dataset, 'genres')
+dataset = preprocess_column(dataset, 'keywords')
+dataset = preprocess_column(dataset, 'cast')
+dataset = preprocess_column(dataset, 'crew')
 
-dataset['cast'].fillna('[]', inplace=True)
-dataset['cast'] = dataset['cast'].apply(lambda x: [i['name'] for i in eval(x)])
-dataset['cast'] = dataset['cast'].apply(lambda x: ' '.join([i.replace(" ", "") for i in x]))
+# Create a new column 'tags' containing the overview, keywords, cast, crew and genres of the movie
+dataset['tags'] = dataset['overview']+' '+dataset['keywords']+' '+dataset['cast']+' '+dataset['crew']+' '+dataset['genres']+' '+dataset['title']
 
-dataset['crew'].fillna('[]', inplace=True)
-dataset['crew'] = dataset['crew'].apply(lambda x: [i['name'] for i in eval(x)])
-dataset['crew'] = dataset['crew'].apply(lambda x: ' '.join([i.replace(" ", "") for i in x]))
+# Drop the unnecessary columns
+dataset.drop(['genres','keywords', 'cast', 'crew', 'overview'], axis=1, inplace=True)
 
-dataset['tags'] = dataset['overview']+' '+dataset['keywords']+' '+dataset['cast']+' '+dataset['crew']+' '+dataset['genre']+' '+dataset['title']
+# Create a set of stopwords
+stop_words = set(stopwords.words("english"))
 
-dataset.drop(['genre','keywords', 'cast', 'crew', 'overview'], axis=1, inplace=True)
+# Initialize the stemmer and lemmatizer
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+def preprocess_tags(tags):
+    if type(tags) == str:
+        # Convert the tags to lowercase
+        tags = tags.lower()
+
+        # Remove punctuation
+        tags = tags.translate(str.maketrans("", "", string.punctuation))
+
+        # Tokenize the tags into words
+        words = nltk.word_tokenize(tags)
+
+        # Remove stopwords and stem/lemmatize the remaining words
+        words = [lemmatizer.lemmatize(stemmer.stem(word)) for word in words if word not in stop_words]
+
+        # Join the processed words back into a string
+        return " ".join(words)
+    else:
+        return tags
+
+# Apply the preprocessing function to the dataset['tags'] column
+dataset['tags'] = dataset['tags'].apply(preprocess_tags)
 
 
-# Vectorizing data
-sw_nltk = stopwords.words('english')
-vectorizer = TfidfVectorizer(max_features=5000, stop_words=sw_nltk)
+''' Content Based Movie Recommender System '''
 
+# Create a TF-IDF vectorizer
+vectorizer = TfidfVectorizer(max_features=5000)
 vectorized_data = vectorizer.fit_transform(dataset['tags'].values.astype('U'))
-vectorizer.get_feature_names_out()
-
 vectorized_dataframe = pd.DataFrame(vectorized_data.toarray(), index=dataset['tags'].index.tolist())
-vectorized_dataframe.shape
 
 
-# Dimentionality Reduction
+# Reduce the dimensionality of the vectorized data
 svd = TruncatedSVD(n_components=1000)
-
 reduced_data = svd.fit_transform(vectorized_dataframe)
-reduced_data.shape
-
-svd.explained_variance_ratio_.cumsum()
 
 
-# Making Recommendations
+# Calculate the cosine similarity between the movies
 reduced_data = reduced_data.astype(np.float32)
 similarity = cosine_similarity(reduced_data)
 
 
+# Function to recommend movies
 def recomendations(movie):
+    # Getting the index of the movie
+    if len(dataset[dataset['title']==movie].index) == 0:
+        print("Movie not found in dataset")
+        return
+        
     id_of_movie = dataset[dataset['title']==movie].index[0]
+    # Calculating cosine similarity between the movie and all other movies
     distances = similarity[id_of_movie]
-    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:15]
+    
+    # Sorting the movies based on cosine similarity
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:11]
+    
+    # Printing the recommended movies
     for movie_id in movie_list:
         print(dataset.iloc[movie_id[0]].title)
 
 
-recomendations('Avengers: Age of Ultron')
+# Taking user input for movie name
+movie_name = input("Enter movie name: ")
+
+# Converting movie name to uppercase
+x = movie_name.upper()
+
+# Calling the recommendation function
+print("Recommended movies:\n",recomendations(x))
